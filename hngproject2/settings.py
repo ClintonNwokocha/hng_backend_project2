16410e91819a50645c4ea5fa038ede0a0bc2afad
Odogwu <leoclinton2011@hotmail.com>
2023-09-11 16:52:49 +0100

    Ready to deploy

--- hngproject2/settings.py
+++ hngproject2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@*r#a9#yftj@imtyh&-8_l)(&8sse$m*xx8xyi670#)7232v%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
